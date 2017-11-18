eee59d19c6478edfc8953542dc734d2b71c537bc
王萍 <wang.03.25@qq.com>
2017-11-18 16:29:33 +0800

    学习域更新器，用于volatile变量，保证其原子性，volatile只保证可见性。

--- jdkTest/src/com/jdk/test/TestAtomicReferenceFieldUpdate.java
+++ jdkTest/src/com/jdk/test/TestAtomicReferenceFieldUpdate.java
@@ -5,16 +5,16 @@ import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 /**
  * Created by 王萍 on 2017/11/18 0018.
  */
-//利用AtomicReferenceFieldUpdater对Node类的volatile修饰的next属性进行了包装，使其具有CAS操作。
+
 public class TestAtomicReferenceFieldUpdate {
 
-    private static AtomicReferenceFieldUpdater<Node, Node> nextUpdate = AtomicReferenceFieldUpdater.newUpdater(Node.class, Node.class, "next");
+    private static final AtomicReferenceFieldUpdater<Node, Node> NEXT_UPDATE = AtomicReferenceFieldUpdater.newUpdater(Node.class, Node.class, "next");
 
     public static void main(String[] args) {
         Node<Integer> integerNode = new Node<Integer>(3);
         Node<Integer> integerNodeNext = new Node<>(4);
         integerNode.next = integerNodeNext;
-        nextUpdate.compareAndSet(integerNode, integerNodeNext, new Node(5));
+        NEXT_UPDATE.compareAndSet(integerNode, integerNodeNext, new Node(5));
         System.out.println(integerNode.next.item);
     }
 }
