cc5f19845c69a36e10c1cd3136b5ca42d9df1c26
王萍 <wang.03.25@qq.com>
2017-12-09 19:03:25 +0800

    HashMap CPU100

--- jdkTest/src/com/jdk/test/TestHashMap.java
+++ jdkTest/src/com/jdk/test/TestHashMap.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.UUID;
 
 /**
  * @author 王萍
@@ -17,7 +18,27 @@ public class TestHashMap {
         HashMap<String, String> map = new HashMap<>();
         Method hash = map.getClass().getDeclaredMethod("hash", Object.class);
         hash.setAccessible(true);
-        int hashOfWanwanpp = (int)hash.invoke(map, "wanwanpp");
+        int hashOfWanwanpp = (int) hash.invoke(map, "wanwanpp");
         System.out.println(Integer.toBinaryString(hashOfWanwanpp));
+    }
+
+    //下面程序会导致cpu100，HashMap
+    public static void main(String[] args) throws InterruptedException {
+        final HashMap<String, String> map = new HashMap<String, String>(2);
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++) {
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            map.put(UUID.randomUUID().toString(), "");
+                        }
+                    }, "ftf" + i).start();
+                }
+            }
+        }, "ftf");
+        t.start();
+        t.join();
     }
 }
